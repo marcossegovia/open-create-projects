263f12106b2db0feccc030a2d23f5e4560e49ae9
Marcos <velozmarkdrea@gmail.com>
2017-01-07 17:16:10 +0100

    Allowing completion depending on current argument

--- open-create-projects.plugin.zsh
+++ open-create-projects.plugin.zsh
@@ -4,17 +4,18 @@ my_golang_projects_path="$HOME/Documents/go-workspace/src/github.com/MarcosSegov
 open_create_projects() {
     local -A projects_path
     projects_path=(
-        php        $my_php_projects_path
-        go        $my_golang_projects_path
+        php      ${my_php_projects_path}
+        go       ${my_golang_projects_path}
     )
-
-    (( $+{projects_path[$1]} )) || return 1
+    
+   #(( $+{projects_path[$1]} )) || return 1
 
     local directory="${projects_path[$1]}/$2"
+    
     if [[ -d "$directory" ]]; then
-        echo "Path '$path' found, opening existing project..."
+        echo "Path '$directory' found, opening existing project..."
     else
-        echo "Path '$dir' not found, creating new project..."
+        echo "Path '$directory' not found, creating new project..."
         mkdir "$directory"
     fi
 
@@ -25,14 +26,21 @@ open_create_projects() {
 }
 
 _open_create_projects() {
+   
     local -A projects_path
     projects_path=(
-        php        ~/Documents/go-workspace/src/github.com/MarcosSegovia
-        go        ~/Documents/vagrant_ubuntu12x32/public
+        php        ${my_php_projects_path}
+        go        ${my_golang_projects_path}
     )
 
-    _arguments ':code platform:(php go)' \
-               '*:project:_files -/ -W $projects_path'
+   case ${words[2]} in
+       go)      
+               _arguments ':code platform:(php go)' \
+               '*:project:_files -/ -W $projects_path[go]';;
+       php)
+               _arguments ':code platform:(php go)' \
+               '*:project:_files -/ -W $projects_path[php]';;
+   esac
 }
 
 compdef _open_create_projects open_create_projects
